fc1ae61de6495db92cbff5900fceb614a9bd784d
nakashimaiori <ioringo072869@gmail.com>
2020-11-22 20:27:08 +0900

    js practice

---
+++ index.js
@@ -0,0 +1,18 @@
+const question = "日本で最も面積の小さい都道府県はどこでしょう？";
+const answers = ["大阪", "香川", "東京", "沖縄"];
+const correct = "香川";
+
+
+document.getElementById("js-question").textContent = question;
+
+const $button = document.getElementByTagName("button");
+
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
+
+
+
+
+document.getElementById("js-btn-1").textContent = answers[0].addEventListener("click", () =>{})
